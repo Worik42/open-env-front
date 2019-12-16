3eb91ea19a1ea2ad8f27388805a9b83184adfd59
Nikolay Vorobev <Worik42@yandex.ru>
2019-12-16 20:58:15 +0700

    change to real backend

--- src/app.tsx
+++ src/app.tsx
@@ -45,7 +45,7 @@ export default class App extends Component {
               <Route path='/login' component={LoginPage} />
               <Route path='/createUser' component={CreateUserPage} />
               <PrivateRoute component={Dashboard} path="/dashboard" />
-              <Redirect path='*' to={'/dashboard'} />
+              <Redirect path='*' to={'/login'} />
             </Switch>
           </Main>
         </ConnectedRouter>
--- src/services/http/config.ts
+++ src/services/http/config.ts
@@ -1,4 +1,4 @@
 export const ACCESS_TOKEN = '@openenv:access_token';
 export const REFRESH_TOKEN = '@gologin:refresh_token';
 
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'http://ov-backend.kemsu.ru';
